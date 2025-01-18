13e21a887c27f9d8e8dcc3a4568884d2f1ee5787
AlissonRM19 <88214936+AlissonRM19@users.noreply.github.com>
2025-01-18 17:08:33 -0600

    [Feat] Conexion entre registro e inicio

--- MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/ExitActivity.java
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/ExitActivity.java
@@ -11,21 +11,21 @@ public class ExitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exit);
+        setContentView(R.layout.registro);
 
         // Configurar la animación de degradado
-        TextView textView = findViewById(R.id.textViewExit);
+        //TextView textView = findViewById(R.id.textViewExit);
         AlphaAnimation fade = new AlphaAnimation(0, 1);
         fade.setDuration(4000); // Aumenta la duración de la animación
         fade.setRepeatMode(AlphaAnimation.REVERSE);
         fade.setRepeatCount(AlphaAnimation.INFINITE);
-        textView.startAnimation(fade);
+        //textView.startAnimation(fade);
 
         // Configurar el botón para salir de la aplicación
-        Button buttonExitApp = findViewById(R.id.buttonExitApp);
-        buttonExitApp.setOnClickListener(view -> {
+        //Button buttonExitApp = findViewById(R.id.buttonExitApp);
+        /*buttonExitApp.setOnClickListener(view -> {
             finishAffinity(); // Cierra esta actividad o pantalla
             System.exit(0); // Sale de la aplicación
-        });
+        });*/
     }
 }
