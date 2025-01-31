5db6e637b653b21fda899b83f35b2b1320e80f6d
Josumveg <josepabloumana@gmail.com>
2025-01-31 12:24:09 -0600

    [feat] Subida local de imagenes

--- MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/Socket.java
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/Socket.java
@@ -1,5 +1,9 @@
 package com.example.miprimeraplicacion;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -9,14 +13,16 @@ public class Socket {
     public static Scanner in;
     public static String message;
     public static boolean sistemaInit = false;
+    public static DataOutputStream dataOut;
 
     public static void iniciarSocket() {
         new Thread(() -> {
             try {
                 // Cambiar a la dirección IP de su servidor
-                socket = new java.net.Socket("192.168.0.106", 1717);
+                socket = new java.net.Socket("192.168.1.24", 1717);
                 out = new PrintWriter(socket.getOutputStream(), true);
                 in = new Scanner(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
                 new Thread(() -> {
                     while (true) {
                         // Escuchar continuamente los mensajes del servidor
@@ -25,7 +31,6 @@ public class Socket {
                             //textViewChat.append("Servidor: " + message + "\n");
                         }
                     }
-
                 }).start();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -44,6 +49,35 @@ public class Socket {
             }
         }).start();
     }
+
+//    public static void sendImages(DataOutputStream dataOutputStream, String[] imagePaths) throws IOException {
+//        dataOutputStream.writeInt(imagePaths.length); // Send number of images
+//
+//        for (String imagePath : imagePaths) {
+//            File imageFile = new File(imagePath);
+//
+//            if (!imageFile.exists()) {
+//                System.out.println("File not found: " + imagePath);
+//                continue;
+//            }
+//
+//            long fileSize = imageFile.length();
+//            dataOutputStream.writeLong(fileSize); // Send image size
+//
+//            try (FileInputStream fileInputStream = new FileInputStream(imageFile)) {
+//                byte[] buffer = new byte[4096];
+//                int bytesRead;
+//
+//                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+//                    dataOutputStream.write(buffer, 0, bytesRead);
+//                }
+//            }
+//
+//            System.out.println("Sent " + imagePath);
+//        }
+//
+//        System.out.println("All images sent successfully!");
+//    }
 
 
 
