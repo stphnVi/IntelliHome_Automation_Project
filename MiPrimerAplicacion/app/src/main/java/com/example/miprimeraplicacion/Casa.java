c705a0927f92d2f69b6a6039c5dd5f17447d17bb
Josumveg <josepabloumana@gmail.com>
2025-02-03 06:11:24 -0600

    [Feat] Guardado de informacion de casas, creacion de interfaz

---
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/Casa.java
@@ -0,0 +1,23 @@
+package com.example.miprimeraplicacion;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Casa {
+    static String precio;
+    static List<String> reglas;
+    static String capacidadMaxima;
+    static List<String> amenidades;
+    static String nombrePropiedad;
+}
+
+class ListaCasas {
+    public static List<Casa> listaCasas = new ArrayList<>();
+}
---
+++ MiPrimerAplicacion/app/src/main/java/com/example/miprimeraplicacion/ResultadosBusqueda.java
@@ -0,0 +1,19 @@
+package com.example.miprimeraplicacion;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class ResultadosBusqueda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultados_busqueda);
+
+
+
+    }
+}
